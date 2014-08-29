1339154be29d58209dffe6a4b6f2db802729b525
Karthik Vasudevan <mithralaya@gmail.com>
2014-08-29 10:55:04 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+var server = require('http').createServer(app).listen(2001);
+var io = require('socket.io').listen(server);
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+io.on('connection', function (socket) {
+
+    socket.on('mouseMove', function (data) {
+        socket.broadcast.emit('mouseMove', {
+            gameCode: data.gameCode,
+            player: data.player,
+            mouseY: data.mouseY
+        });
+    });
+});
+
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+
+module.exports = app;
---
+++ public/javascripts/global.js
@@ -0,0 +1,132 @@
+/**
+ * Created by Karthik on 28/08/2014.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+var context;
+var x = (window.innerWidth-50)/2;
+var y = (window.innerHeight-50)/2;
+var dx = 5;
+var dy = 5;
+
+var player1BatX = 108;
+var player1BatY = 300;
+var player2BatX = (document.body.offsetWidth - 107);
+var player2BatY = 500;
+var player = 2;
+var gameCode;
+var socket = io.connect('http://localhost:2001');
+
+var player1Score = 0;
+var player2Score = 0;
+
+
+var myCanvas = document.getElementById("myCanvas");
+var player1BatObject = document.getElementById("player1Bat");
+var player2BatObject = document.getElementById("player2Bat");
+
+
+function init()
+{
+    player1Bat();
+    player2Bat();
+    setInterval(draw,10);
+}
+
+$(document).ready(function(){
+    $("#player").change(function(){
+        player = $("#player").val();
+    });
+    $("#myCanvas").mousemove(function(e){
+        if(parseInt(e.pageY) > 170 && parseInt(e.pageY) < (document.body.offsetHeight - 108)) {
+            if(player == 1)
+            {
+                player1BatObject.style.top = e.pageY + "px";
+                player1BatY = e.pageY;
+            }
+            if(player == 2)
+            {
+                player2BatObject.style.top = e.pageY + "px";
+                player2BatY = e.pageY;
+            }
+            var data = {
+                "gameCode": gameCode,
+                "player": player,
+                "mouseY": e.pageY
+            }
+            socket.emit('mouseMove', data);
+        }
+    });
+});
+
+function player1Bat()
+{
+    player1BatObject.style.top = player1BatY+"px";
+    player1BatObject.style.left = player1BatX+"px";
+}
+function player2Bat()
+{
+    player2BatObject.style.top = player2BatY + "px";
+    player2BatObject.style.left = player2BatX + "px";
+}
+
+
+
+function draw()
+{
+    context = myCanvas.getContext('2d');
+    context.beginPath();
+    context.canvas.width  = window.innerWidth-50;
+    context.canvas.height = window.innerHeight-50;
+    context.fillStyle = "#111000";
+    context.fillRect(100,100,(document.body.offsetWidth - 200),(document.body.offsetHeight - 200));
+    context.fill();
+    context.moveTo((document.body.offsetWidth/2), 100);
+    context.lineTo((document.body.offsetWidth/2), (document.body.offsetHeight -100));
+    context.closePath();
+    context.strokeStyle = '#ffffff';
+    context.stroke();
+    context.closePath();
+    context.fillStyle = "#00ff00";
+    context.arc(x, y, 13, 0, Math.PI*2, true);
+    context.fill();
+    context.closePath();
+
+    if(player = 1) {
+        if (x < 120) {
+            if(y >= player1BatY - 115 && y <= (player1BatY + 80 - 115))
+            {
+                dx = -dx;
+            }
+        }
+    }
+    if(player = 2){
+        if (x > (document.body.offsetWidth - 120)  ) {
+            if(y >= player2BatY - 115 && y <= (player2BatY + 80 - 115))
+            {
+                dx = -dx;
+            }
+        }
+    }
+
+    if(y < 115 || y > document.body.offsetHeight - 115)
+    {
+        dy = -dy;
+    }
+    x += dx;
+    y += dy;
+    context.restore();
+}
+
+socket.on('mouseMove', function (data) {
+    if(data.player == 1)
+    {
+        player1BatObject.style.top = data.mouseY + "px";
+    }
+    if(data.player == 2)
+    {
+        player2BatObject.style.top = data.mouseY + "px";
+    }
+});
